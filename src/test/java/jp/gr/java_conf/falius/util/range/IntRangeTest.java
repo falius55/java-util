bbcda011ad0169cf47db02552a059c68ed6e5ca4
ymiyauchi <ciero639@gmail.com>
2017-03-17 14:36:37 +0900

    add iteratorTest@IntRangeTest

--- src/test/java/jp/gr/java_conf/falius/util/range/IntRangeTest.java
+++ src/test/java/jp/gr/java_conf/falius/util/range/IntRangeTest.java
@@ -1,18 +1,34 @@
 package jp.gr.java_conf.falius.util.range;
 
+import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
 
 public class IntRangeTest {
+
     @Test
-    public void rangeTest() {
+    public void iteratorTest() {
+        int start = -5;
+        int end = 29;
+        int step = 2;
+        int k = start;
+        for (int i : new IntRange(k, end, step)) {
+            assertThat(i, is(k));
+            k += step;
+        }
+        assertTrue(k >= end);
+        assertTrue(k < end + step);
+    }
+
+    @Test
+    public void toArrayTest() {
         int start = -5;
         int end = 29;
         int step = 2;
         int k = start;
         for (int i : new IntRange(k, end, step).toArray()) {
-            assertEquals(i, k);
+            assertThat(i, is(k));
             k += step;
         }
         assertTrue(k >= end);
