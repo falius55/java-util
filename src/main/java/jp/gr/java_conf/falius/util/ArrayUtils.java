5216b7b707b7300bb85b39c863a8e2d90a469b82
ymiyauchi <ciero639@gmail.com>
2017-12-29 13:28:16 +0900

    add class ArrayUtils

---
+++ src/main/java/jp/gr/java_conf/falius/util/ArrayUtils.java
@@ -0,0 +1,22 @@
+package jp.gr.java_conf.falius.util;
+
+public final class ArrayUtils {
+
+    private ArrayUtils() { }
+
+    public static int[] toIntArray(Integer[] integers) {
+        int[] ret = new int[integers.length];
+        for (int i = 0, size = integers.length; i < size; i++) {
+            ret[i] = integers[i].intValue();
+        }
+        return ret;
+    }
+
+    public static Integer[] toIntegerArray(int[] ints) {
+        Integer[] ret = new Integer[ints.length];
+        for (int i = 0, size = ints.length; i < size; i++) {
+            ret[i] = Integer.valueOf(ints[i]);
+        }
+        return ret;
+    }
+}
